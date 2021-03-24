79b7c14ab1ff8d83daa92f8a1a7fb470279ecda0
yuvalYah <yuvalpe000@gmail.com>
2021-03-24 14:41:46 +0200

    first virson

---
+++ Board.cpp
@@ -0,0 +1,24 @@
+#ifndef BOARD_H
+#define BOARD_H
+#include "Board.hpp"
+using namespace std;
+
+namespace ariel {
+	// Defines a direction of a message on the board
+
+    void Board::post(unsigned  int x ,unsigned  int y , Direction d ,string const& s){
+
+    }
+    string Board::read(unsigned int x ,unsigned  int y , Direction d , int len){
+        t=0;
+        
+        return "";
+    }
+    string Board::show() {
+        t=0;
+        return "";
+    }
+	
+};
+#endif
+
